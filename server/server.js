6e55013508b2cd8ab68ae4c30c016cd16d110e12
Eric Cheni <sweetdream0828@gmail.com>
2025-01-29 16:50:43 -0800

    Test mongodb

--- server/server.js
+++ server/server.js
@@ -1,13 +1,11 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const uri =
   "mongodb+srv://ericchen:rarara96828@cluster0828eric.x6euv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0828eric";
 
 dotenv.config();
 
 mongoose
-  .connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(uri)
   .then(() => console.log("MongoDB Atlas Connected"))
   .catch((err) => console.error("MonogoDB connection error; ", err));
